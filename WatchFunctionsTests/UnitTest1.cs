1c9c4e591fed9bc44c903141d79ff9e9fb777c6a
Hasan Othman <h.othman@milestone-consult.de>
2021-06-07 14:53:02 +0200

    live xUnit azure function

---
+++ WatchFunctionsTests/UnitTest1.cs
@@ -0,0 +1,41 @@
+using System;
+using Xunit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace WatchFunctionsTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestWatchFunctionSuccess()
+        {
+            var queryStringValue = "abc";
+            var request = new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Query = new QueryCollection
+                (
+                    new System.Collections.Generic.Dictionary<string, StringValues>()
+                    {
+                       { "model", queryStringValue }
+                    }
+                )
+            };
+            var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+            var response = WatchPortalFunction.WatchInfo.Run(request, logger);
+            response.Wait();
+
+            Assert.IsAssignableFrom<OkObjectResult>(response.Result);
+
+            // Check that the contents of the response are the expected contents
+            var result = (OkObjectResult)response.Result;
+            dynamic watchinfo = new { Manufacturer = "abc", CaseType = "Solid", Bezel = "Titanium", Dial = "Roman", CaseFinish = "Silver", Jewels = 15 };
+            string watchInfo = $"Watch Details: {watchinfo.Manufacturer}, {watchinfo.CaseType}, {watchinfo.Bezel}, {watchinfo.Dial}, {watchinfo.CaseFinish}, {watchinfo.Jewels}";
+            Assert.Equal(watchInfo, result.Value);
+        }
+    }
+}
